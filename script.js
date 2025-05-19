dc9724bd45e1090e3a1bb01dd4ce5e659477834c
Chanchal253 <chanchal327@gmail.com>
2025-05-20 01:43:54 +0530

    adding

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const hourEl = document.getElementById("hour");
+const hourEl = document.getElementById("hour");         
 const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
+const secondEl = document.getElementById("seconds"); //for 00s(to get tags need to work on )
 const ampmEl = document.getElementById("ampm");
-function updateClock(){
+function updateClock(){                                 // fn creation
     let h = new Date().getHours();
-    let m = new Date().getMinutes();
-    let s = new Date().getSeconds();
+    let m = new Date().getMinutes();      // fn to adopt new time
+    let s = new Date().getSeconds();   
     let ampm = "AM";
     if(h > 12) {
-        h = h-12;
+        h = h-12;                        // for am pm changing
         ampm = "PM";
     }
     h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
+    m = m < 10 ? "0" + m : m;             // to add double digits
     s = s < 10 ? "0" + s : s;
 
 
@@ -21,7 +21,7 @@ function updateClock(){
 
     hourEl.innerText = h;
     minuteEl.innerText = m;
-    secondEl.innerText = s;
+    secondEl.innerText = s;              // to print the upper fn
     ampmEl.innerText = ampm;
     setTimeout(()=>{
         updateClock()
@@ -31,7 +31,8 @@ function updateClock(){
 
 }
 
-updateClock();
+updateClock();                           // for repeating code 
+ 
 
 
 
